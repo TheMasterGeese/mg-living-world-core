1359a56ea23efe2cab9936bee2267f9b9f70eaca
TheMasterGeese <themastergeese@gmail.com>
2022-07-22 21:37:20 -0400

    iterating on proxy gm

--- src/mg-living-world-core.ts
+++ src/mg-living-world-core.ts
@@ -1,26 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * Register all settings
  */
- Hooks.once("init", function () {
 
-	game.settings.register("mg-living-world-core", "GM Proxy", {
+
+Hooks.once("ready", function () {
+    // We can't set up these fields that require user selection until the "ready" hook, since users haven't been initialized yet.
+    const gmUsers = game.users.filter((user : User) => user.isGM);
+    const gmChoices :{[name : string]: string}= {};
+    gmUsers.forEach((gmUser : User) => {
+        gmChoices[gmUser.id] = gmUser.name
+    });
+    game.settings.register("mg-living-world-core", "GMProxy", {
 		name: game.i18n.localize("CORE.GMProxyTitle"),
 		hint: game.i18n.localize("CORE.GMProxyHint"),
 		scope: "world",
 		config: true,
-		type: Number,
+        choices: gmChoices as never,
+		type: String,
 	});
-    
-});
-
-Hooks.once("ready", function () {
-    // We can't set up the choices until the "ready" hook, since users haven't been initialized yet.
-    const gmProxySetting = game.settings.get("mg-living-world-core", "GM Proxy") as InexactPartial<Omit<SettingConfig<number>, "key" | "namespace">>
-    const gmUsers = game.users.filter((user : User) => user.isGM);
-    const gmChoices :{[name : string]: string}= {};
-    for (let i = 0; i < gmUsers.length; i++) {
-        gmChoices[i] = gmUsers[i].name
-    }
-    gmProxySetting.choices = gmChoices as never;
-
 })
