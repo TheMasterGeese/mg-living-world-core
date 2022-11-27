783f4ffe0f6fd37c440c77a7df6c7dec1a8f66b6
TheMasterGeese <themastergeese@gmail.com>
2022-11-27 12:34:38 -0500

    linter fixes

--- src/mg-living-world-core.ts
+++ src/mg-living-world-core.ts
@@ -1,13 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * Register all settings
  */
-
-
 Hooks.once("ready", function() {
-  // We can't set up these fields that require user selection until the "ready" hook, since users haven't been initialized yet.
+  /**
+   * We can't set up these fields that require user selection until the "ready" hook, since users haven't been
+   * initialized yet.
+   */
   const gmUsers = game.users.filter((user : User) => user.isGM);
   const gmChoices :{[name : string]: string}= {};
   gmUsers.forEach((gmUser : User) => {
